1c3ae2435e613f1bdca530fa32663f5cdd339be0
khushboo-totality <khushboo@totality.solutions>
2026-07-07 12:28:21 +0530

    added volaris pdf

--- components/sections/home/ProductGridSection.tsx
+++ components/sections/home/ProductGridSection.tsx
@@ -17,7 +17,7 @@ const NEW_ARRIVALS = [
   { id: 2, img: '/images/home/product/Indoor_Catalogue.jpg', title: 'Indoor', href:"/product/indoor", pdf:"/pdf/INDOOR.pdf" },
   { id: 3, img: '/images/home/product/Artizan_Catalogue.jpg', title: 'Artizan', href:"/product/artizan", pdf:"/pdf/ARTIZAN.pdf" },
   { id: 4, img: '/images/home/product/Astara_Catalogue.jpg', title: 'Astara', href:"/product/astara" },
-  { id: 5, img: '/images/home/product/Volaris_Catalogue.jpg', title: 'Volaris', href:"/product/volaris" },
+  { id: 5, img: '/images/home/product/Volaris_Catalogue.jpg', title: 'Volaris', href:"/product/volaris", pdf:"/pdf/VOLARIS.pdf" },
   { id: 6, img: '/images/home/product/Klewe_Catalogue.jpg', title: 'Klewe', href:"/product/klewe", pdf:"/pdf/KLEWE.pdf" },
 ];
 
